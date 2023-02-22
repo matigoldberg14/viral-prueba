808208950027303fc80446c6bd1df7cbf5edfdc6
matigoldberg14 <98336460+matigoldberg14@users.noreply.github.com>
2023-02-22 14:35:00 -0300

    Updates

---
+++ precache-manifest.29c6718430f407801cbb33b8acf3545d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb442adc01cee64e9183",
+    "url": "/viral-prueba/static/css/main.676917d6.chunk.css"
+  },
+  {
+    "revision": "eb442adc01cee64e9183",
+    "url": "/viral-prueba/static/js/main.eb442adc.chunk.js"
+  },
+  {
+    "revision": "e4d3a95ef83e012c812b",
+    "url": "/viral-prueba/static/js/1.e4d3a95e.chunk.js"
+  },
+  {
+    "revision": "9d96fd371aa4d4d185be",
+    "url": "/viral-prueba/static/js/runtime~main.9d96fd37.js"
+  },
+  {
+    "revision": "b7b4358ae8a9c93041e864e48a70e43a",
+    "url": "/viral-prueba/static/media/logoGrande.b7b4358a.png"
+  },
+  {
+    "revision": "8a2bb870608b567c2d1857fbdb94f44e",
+    "url": "/viral-prueba/static/media/cuadroGrande.8a2bb870.jpg"
+  },
+  {
+    "revision": "6da54345fd7567750f75f5ca4d01eb71",
+    "url": "/viral-prueba/index.html"
+  }
+];
