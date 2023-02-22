6e7482190798ae3a67de4b29f15b6b7c901b2af6
matigoldberg14 <98336460+matigoldberg14@users.noreply.github.com>
2023-02-22 14:38:42 -0300

    Deployed Webiste

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React from "react";
 import { Navbar } from "./components/Navbar";
 import { CuadroPrincipal } from "./components/CuadroPrincipal";
 import "./app.css";
+import OnramperWidget from "@onramper/widget";
 
+ function AppPrueba() {
+  return (
+    <div
+      style={{
+        width: "440px",
+        height: "595px",
+        boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.1)",
+        borderRadius: "10px",
+        margin: "auto"
+      }}
+    >
+      <OnramperWidget
+        defaultAmount={200}
+        API_KEY="pk_test_ass3gtLSWQpI11IWUZLJdrfyQhj7bTw_3xwLvhEvH6Q0"
+        filters={{
+          onlyCryptos: ["ETH"]
+        }}
+      />
+    </div>
+  );
+}
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <CuadroPrincipal/>
+      <AppPrueba/>
     </div>
   );
 };
